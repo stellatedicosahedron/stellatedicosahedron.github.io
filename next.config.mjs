6ea90225f84bd63309589e1219399334d2f84f5b
Ben Li <cben.li@Amail.utoronto.ca>
2024-05-24 13:39:47 -0400

    more troubleshooting

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
-/**
-* @type {import('next').NextConfig}
-*/
-const nextConfig = {
-    output: "export",
-    images: {
-      loader: "akamai",
-      path: "",
-    },
-    assetPrefix: "./",
-  };
-  
-  export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
